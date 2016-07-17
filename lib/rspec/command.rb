ccd4d438c5f29dde45f580699ceabfe9167fb71f
autopp <autopp@gmail.com>
2016-07-17 23:56:47 +0900

    impl methods of Command

--- lib/rspec/command.rb
+++ lib/rspec/command.rb
@@ -7,11 +7,43 @@ module Rspec
   # @author atanimur <atanimur@yahoo-corp.jp>
   #
   class Command
+    # @!attribute [r] stdout
+    #   @return [String,nil]
+    #     Standard output if given command is already executed, otherwise nil
+    attr_reader :stdout
+
+    # @!attribute [r] stderr
+    #   @return [String]
+    #     Standard error if given command is already executed, otherwise nil
+    attr_reader :stderr
+
+    # @!attribute [r] status
+    #   @return [Process::Status]
+    #     Status of given command if already executed, otherwise nil
+    attr_reader :status
+
     #
     # @param [String] wrapped command string
     #
-    def initialize(command)
-      @command = command.to_s
+    def initialize(cmd)
+      @cmd = cmd.to_s
+      @stdout = @stderr = @status = nil
+    end
+
+    def execute
+      execute! unless executed?
+      self
+    end
+
+    def execute!
+      @stdout, @stderr, @status = Open3.capture3(@cmd)
+      self
+    end
+
+    private
+
+    def executed?
+      !@status.nil?
     end
   end
 end
