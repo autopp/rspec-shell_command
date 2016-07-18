61485bdd17e4085e2828f3a9848acc211c3ae412
autopp <autopp@gmail.com>
2016-07-18 20:56:31 +0900

    fix line length

--- lib/rspec/command/matchers/base.rb
+++ lib/rspec/command/matchers/base.rb
@@ -10,7 +10,8 @@ module RSpec
       class Base
         def matches?(actual)
           unless actual.is_a?(RSpec::Command)
-            raise TypeError, "expected RSpec::Command, but got #{actual.inspect}"
+            raise TypeError,
+                  "expected RSpec::Command, but got #{actual.inspect}"
           end
           @actual = actual
           perform_match(actual.execute)
