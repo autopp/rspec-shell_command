a947c743a8b0e85e89ecd1c8e7d15991758ff78f
autopp <autopp@gmail.com>
2016-07-18 13:09:56 +0900

    add comment

--- lib/rspec/command/matchers/success.rb
+++ lib/rspec/command/matchers/success.rb
@@ -1,6 +1,12 @@
 module RSpec
   class Command
     module Matchers
+      # Provide implementation of success matcher
+      #
+      # Check whether given command successed
+      #
+      # @author atanimur <atanimur@yahoo-corp.jp>
+      #
       class Success
         def matches?(actual)
           actual.execute.status.success?
