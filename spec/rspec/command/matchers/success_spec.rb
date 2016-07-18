98eecdee8ab0b05c4d17c9caa74415fb265fc730
autopp <autopp@gmail.com>
2016-07-18 12:32:34 +0900

    fix description

--- spec/rspec/command/matchers/success_spec.rb
+++ spec/rspec/command/matchers/success_spec.rb
@@ -4,13 +4,13 @@ describe 'success matcher' do
   include RSpec::Command::Matchers
 
   context 'when given a "echo hello"' do
-    it 'passes' do
+    it 'matches' do
       expect(RSpec::Command.new('echo hello')).to success
     end
   end
 
   context 'when given a "eho goodby; exit 1"' do
-    it 'fails' do
+    it 'dose not matches' do
       expect(RSpec::Command.new('echo goodby; exit 1')).not_to success
     end
   end
